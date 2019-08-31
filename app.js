7aa78adcd97d0da8edd875ae62c46a9d8a37ad87
dantehsema@yahoo.com <dantehsema@yahoo.com>
2019-08-30 20:53:26 -0400

    added problem statement

---
+++ app.js
@@ -0,0 +1,22 @@
+/*
+1a) Create an app that manages the class enrollment for a given course. 
+The app should collect data regarding  
+- student name
+- student id
+- pay status 
+
+1b) Your app should add students to an enrollment list in the following format
+name    student id      paid    
+carine  3480239         paid    
+marley  0238034         unpaid
+
+1c) Your app should provide functionality to edit and delete students on the list
+name    student id      paid    edit        delete
+carine  3480239         paid    edit icon   trash icon
+marley  0238034         unpaid  edit icon   trash icon
+
+2a) Limit the class size of your list to 5 students. Students added after 
+the class is full should be added to class waitlist
+
+2b) Add a drop down to your form that lets you select a students major. The 
+*/
