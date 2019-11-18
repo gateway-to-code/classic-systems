8fb25ef88b52b3510936ce80ffdd3d805208dcfc
dantehsema@yahoo.com <dantehsema@yahoo.com>
2019-11-18 18:43:39 -0500

    change code

--- app.js
+++ app.js
@@ -24,43 +24,88 @@ the class is full should be added to class waitlist
 2b) Add a drop down to your form that lets you select a students major. The 
 */
 
-
+let editRow = null;
 
 submitForm = function (event) {
-    console.log('this is the this', this);
     event.preventDefault();
-    let formData = {}
+    let formData = {};
     console.log('this is the event ', event);
     formData['name'] = document.getElementById('name').value;
-    formData['studentId'] = document.getElementById('student_id').value
+    formData['studentId'] = document.getElementById('student_id').value;
     formData['payStatus'] = this.checkPayment(document.getElementsByName('payStatus'));
-    console.log('this is the name', formData);
-    insertData(formData);
+    console.log('this is the pay status', this.checkPayment(document.getElementsByName('payStatus')));
+    if(editRow === null){
+        insertData(formData);
+    }else{
+        updateData(formData);
+    }
+    reset();
 
 }
 
-checkPayment = function(element) {
-    console.log()
-    for(let i=0; i <= element.length; i++) {
-        if(element[i].checked) {
-            return element[i].value;
+checkPayment = function(elements) {
+    console.log('this is the pay status', elements)
+    for(let i=0; i < elements.length; i++) {
+        if(elements[i].checked) {
+            elements[i].checked = false;
+            return elements[i].value;
         }
     }
 }
 
 insertData = function(data) {
     let table = document.getElementById('registration_table');
-    console.log('this is the table ', table);
     let newRow = table.insertRow(0);
-    console.log('new Row', newRow, table);
-    var cell1 = newRow.insertCell(0);
-    var cell2 = newRow.insertCell(1);
-    var cell3 = newRow.insertCell(2);
+    let cell1 = newRow.insertCell(0);
+    let cell2 = newRow.insertCell(1);
+    let cell3 = newRow.insertCell(2);
+    let cell4 = newRow.insertCell(3);
 
-
-    // Add some text to the new cells:
     cell1.innerHTML = data.name;
     cell2.innerHTML = data.studentId;
     cell3.innerHTML = data.payStatus;
+    cell4.innerHTML = '<a onClick="editField(this)">Edit</a> | <a onClick="deleteField(this)">Delete</a>';
+
+
+}
+
+editField = function(element) {
+    editRow = element.parentElement.parentElement.childNodes;
+    console.log('this is the selected element', editRow);
+    document.getElementById('name').value = editRow[0].innerText;
+    document.getElementById('student_id').value = editRow[1].innerText;
+    // document.getElementsByName('payStatus') = checkPayStatus(editRow[2].innerText);
+    let payStatusFromTable = editRow[2].innerText
+    checkPayStatus(payStatusFromTable);
+
+}
+
+deleteField = (element) => {
+    let parent = document.getElementById('registration_table');
+    let child = element.parentElement.parentElement;
+    console.log('this is the parent', parent.childNodes);
+    parent.removeChild(child);
+}
+
+checkPayStatus = (txt) => {
+    console.log("This is payStatusTxt", txt);
+    let formRadio = document.getElementsByName('payStatus');
+    for(let i=0; i<formRadio.length; i++){
+        console.log('this formRadio text', formRadio[i].value);
+        if(txt === formRadio[i].value){
+            formRadio[i].checked = true;
+        }
+    }
+}
+
+updateData = (data) => {
+    editRow[0].innerHTML = data.name;
+    editRow[1].innerHTML = data.studentId;
+    editRow[2].innerHTML = data.payStatus;
+    editRow = null;
+}
+reset = function() {
+    document.getElementById('name').value = '';
+    document.getElementById('student_id').value = '';
 
 }
